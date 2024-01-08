04dcb88071533089f48f2dd90c60e8efc32e7609
Tim <github@timgreen.ws>
2024-01-08 17:49:59 +1100

    created footer centered pattern

---
+++ patterns/footer-centered.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Footer Centered
+ * Slug: footer-centered
+ * Description:
+ * Categories: footer
+ * Keywords:
+ * Viewport Width: 1500
+ * Block Types: core/template-part/footer
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"},"blockGap":"var:preset|spacing|large"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"main","textColor":"base","className":"dark-footer","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull dark-footer has-base-color has-main-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--xx-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"textAlign":"center"} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size"><!-- wp:paragraph -->
+<p>Download</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Visit Forest</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Twitter</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Visit Like</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center","textColor":"main-accent","fontSize":"x-small"} -->
+<p class="has-text-align-center has-main-accent-color has-text-color has-x-small-font-size">© 2023 · Powered by WordPress and <a href="#">Forest</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"main","iconColorValue":"#150E29","iconBackgroundColor":"base","iconBackgroundColorValue":"#fff","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}},"className":"is-style-default","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
