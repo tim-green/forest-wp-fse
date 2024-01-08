51b2142a2d85b4ea6af2f5b395b62f12fc607ddc
Tim <github@timgreen.ws>
2024-01-08 17:50:03 +1100

    created footer light pattern

---
+++ patterns/footer-light.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Title: Footer Light
+ * Slug: footer-light
+ * Description:
+ * Categories: footer
+ * Keywords:
+ * Viewport Width: 1500
+ * Block Types: core/template-part/footer
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|xx-large","right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"},"margin":{"top":"0px"},"blockGap":"var:preset|spacing|large"},"elements":{"link":{"color":{"text":"var:preset|color|main"}}}},"backgroundColor":"tertiary","textColor":"main","className":"dark-footer","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull dark-footer has-main-color has-tertiary-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--xx-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-large","left":"var:preset|spacing|x-large"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Ride with Forest</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Easily create beautiful, fully-customisable websites with the new WordPress Site Editor and the forest block theme. No coding skills required. Download for free today!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#fff","iconBackgroundColor":"main","iconBackgroundColorValue":"#14111f","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}},"className":"is-style-default","layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Company</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size"><!-- wp:paragraph -->
+<p>About</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Careers</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Brand Assets</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Resources</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size"><!-- wp:paragraph -->
+<p>Blog</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Support Docs</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Get Help</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-style:normal;font-weight:600">Product</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size"><!-- wp:paragraph -->
+<p>Features</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Pricing</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Use Cases</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Demo</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"40px"}},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary"} -->
+<div class="wp-block-group alignwide has-secondary-color has-text-color has-link-color" style="padding-top:40px"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"x-small"} -->
+<p class="has-x-small-font-size">© 2023 · Powered by WordPress and <a href="#">Forest</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"x-small"} -->
+<div class="wp-block-group has-x-small-font-size"><!-- wp:paragraph -->
+<p>Download</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Visit forest</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Visit Mike</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
