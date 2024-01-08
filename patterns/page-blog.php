87764b60957cd6d8f400df457677a2bd8944d029
Tim <github@timgreen.ws>
2024-01-08 17:51:56 +1100

    created blog page pattern

---
+++ patterns/page-blog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Blog Page
+ * Slug: forest/blog-page
+ * Description: A full page design for a blog page
+ * Categories: forest/pages
+ * Keywords: page, layout, design, template, blog, posts, query
+ * Viewport Width: 1500
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:pattern {"slug":"forest/post-loop-grid"} /-->
