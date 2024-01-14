f02c28c42f27bef196a7efab561a6bf4b2225564
Tim <github@timgreen.ws>
2024-01-15 09:41:51 +1100

    added block style variations.

--- inc/setup.php
+++ inc/setup.php
@@ -51,3 +51,76 @@ if ( ! function_exists( 'grnd_setup_theme' ) ) {
 }
 
 add_action( 'after_setup_theme', 'grnd_setup_theme' );
+/**
+ * Add block style variations.
+ */
+function register_block_styles() {
+
+	$block_styles = array(
+		'core/button'                    => array(
+			'secondary-button' => __( 'Secondary', 'forest' ),
+		),
+		'core/list'                      => array(
+			'list-check'        => __( 'Check', 'forest' ),
+			'list-check-circle' => __( 'Check Circle', 'forest' ),
+			'list-boxed'        => __( 'Boxed', 'forest' ),
+		),
+		'core/query-pagination-next'     => array(
+			'wp-block-button__link' => __( 'Button', 'forest' ),
+		),
+		'core/query-pagination-previous' => array(
+			'wp-block-button__link' => __( 'Button', 'forest' ),
+		),
+		'core/code'                      => array(
+			'dark-code' => __( 'Dark', 'forest' ),
+		),
+		'core/cover'                     => array(
+			'blur-image-less' => __( 'Blur Image Less', 'forest' ),
+			'blur-image-more' => __( 'Blur Image More', 'forest' ),
+			'rounded-cover'   => __( 'Rounded', 'forest' ),
+		),
+		'core/column'                    => array(
+			'column-box-shadow' => __( 'Box Shadow', 'forest' ),
+		),
+		'core/post-excerpt'              => array(
+			'excerpt-truncate-2' => __( 'Truncate 2 Lines', 'forest' ),
+			'excerpt-truncate-3' => __( 'Truncate 3 Lines', 'forest' ),
+			'excerpt-truncate-4' => __( 'Truncate 4 Lines', 'forest' ),
+		),
+		'core/group'                     => array(
+			'column-box-shadow' => __( 'Box Shadow', 'forest' ),
+		),
+		'core/separator'                 => array(
+			'separator-dotted' => __( 'Dotted', 'forest' ),
+			'separator-thin'   => __( 'Thin', 'forest' ),
+		),
+		'core/image'                     => array(
+			'rounded-full' => __( 'Rounded Full', 'forest' ),
+			'media-boxed'  => __( 'Boxed', 'forest' ),
+		),
+		'core/preformatted'              => array(
+			'preformatted-dark' => __( 'Dark Style', 'forest' ),
+		),
+		'core/post-terms'                => array(
+			'term-button' => __( 'Button Style', 'forest' ),
+		),
+		'core/video'                     => array(
+			'media-boxed' => __( 'Boxed', 'forest' ),
+		),
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\register_block_styles' );
+
+
